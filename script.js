b8182eabbcd9fd25961f3e51dd1c48b3373be8e1
Davide Gonnelli <davide.gonnelli1@gmail.com>
2024-12-11 09:31:34 +0100

    evenorodd

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 /*
+            PALINDROMA
 Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
 */
@@ -29,3 +30,25 @@ const checkIsPalindrome = isPalindrome(userWord);
  } else {
     console.log("la parola non è palindroma");
  }
+
+
+
+ /*
+        PARI O DISPARI
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto.
+*/
+
+const userEvenOrOdd = prompt("Inserisci pari o dispari");
+const userNumber = Number(prompt("Inserisci un numero fra 1 e 5"));
+const cpuNumber = Math.floor(Math.random() * 5) + 1;
+const sum = userNumber + cpuNumber;
+
+if((sum % 2 === 0 && userEvenOrOdd === 'pari') || (sum % 2 === 1 && userEvenOrOdd === 'dispari')) {
+    alert("Hai vinto");
+} else {
+    alert("Hai perso");
+}
